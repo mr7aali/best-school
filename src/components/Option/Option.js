b952a4430f92722dd9eb35c583b49b9ea33f917d
mr7aali <mr7aali@gmail.com>
2022-10-12 05:26:43 +0600

    error

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
 import "./Option.css"
 const Option = ({ option,correctAnswer}) => {
-    const [answer,setAnswer]= useState(false);
+
+  
+    // const [answer,setAnswer]= useState();
+    let name;
+    
     const answerTester =(clickED)=>{
-        setAnswer(correctAnswer===clickED);
+       
+        // setAnswer(correctAnswer===clickED);
+           // console.log(correctAnswer===clickED);
+            if(correctAnswer===clickED){
+                name='bg-blue';
+            }
+            else{
+                name='bg-red'
+            }
     }
+    
 
 
     return (
-        <div onClick={()=>answerTester(option)} className={answer? `bg-blue` : 'bg-red'}>
+        <div onClick={()=>answerTester(option)} className={'bg-common'}>
             
             <p>{option}</p>
         </div>
