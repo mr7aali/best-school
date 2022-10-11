b74ac8a26e8a48b407ad5054417321cae2c61eda
mr7aali <mr7aali@gmail.com>
2022-10-11 22:33:35 +0600

    fjixing err

---
+++ src/components/HomeBlog/HomeBlog.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './HomeBlog.css'
+const HomeBlog = () => {
+    return (
+        <div className='home-blog-container'>
+            <div className='picture-div'>
+                <img src='https://i.ibb.co/hd6SqyF/john-schnobrich-2-FPjl-Ay-MQTA-unsplash.jpg' alt="" />
+            </div>
+            <div className='home-blog'>
+                <h2>15 Software Engineering Riddles to Quiz Your Friends and Co-workers</h2>
+                <p>Software engineering allows developers to build amazing products from video streaming services like YouTube to social networking sites like LinkedIn along with the plethora of apps and games on the Apple App Store and Google Play Store. Here are 15 “What am I?” software engineering riddles to share with friends and any software engineers you know.</p>
+            </div>
+        </div>
+    );
+};
+
+export default HomeBlog;
