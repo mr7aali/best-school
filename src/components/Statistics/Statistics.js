b4bdffc2bc29854a106fb6cf239c26b31781a046
mr7aali <mr7aali@gmail.com>
2022-10-12 20:06:18 +0600

    chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import Total from '../TotalSta/Total';
 import './Statistics.css'
+
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+
+
 const Statistics = () => {
-   const Topic = useLoaderData();
-   console.log(Topic.data)
+    const data= useLoaderData();
+    
     return (
-        <div>
-            {
-                Topic.data.map(topic=> <Total
-                name= {topic.name}
-                total={topic.total}
-                ></Total>)
-            }
+        <div className='chart'>
+
+            <LineChart
+                width={900}
+                height={400}
+                data={data.data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+               
+                <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+            </LineChart>
+
+
         </div>
     );
 };
